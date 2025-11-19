612cd3e319d62c6aab081cc3bb80ce741dbc5e69
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-19 21:58:22 +0530

    4th

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} alt="" className='w-36' />
-      <ul className='sm:flex gap-5 text-sm text-gray-700 hidden'>
+      <Link to='/'><img src={assets.logo} alt="" className='w-36' /></Link>
+      <ul className='sm:flex gap-5 text-sm text-gray-700'>
 
         <NavLink
           to="/"
           className="flex flex-col items-center gap-1 group cursor-pointer"
         >
           <p>Home</p>
-          <hr className="w-0 h-[2px] bg-gray-700 border-none transition-all duration-300 group-hover:w-2/4 hidden" />
+          <hr className="w-0 h-[2px] bg-gray-700 border-none transition-all duration-300 group-hover:w-2/4 " />
         </NavLink>
 
         <NavLink
