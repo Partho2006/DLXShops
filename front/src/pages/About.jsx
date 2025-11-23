cf1b231f278401f7ee34f8c12adefd134bbf7e3a
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-23 19:40:16 +0530

    12th

--- front/src/pages/About.jsx
+++ front/src/pages/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import Title from '../components/Title'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='border-t'>
+      <div className="text-2xl text-center pt-8">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-8 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} alt="" className='w-full md:max-w-[450px]' />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p className="">We are a contemporary fashion label committed to designing premium jeans, shirts, and modern essentials that merge refined craftsmanship with everyday comfort, giving you style that feels effortless and lasts far beyond the trends.</p>
+          <p className="">We deliver contemporary jeans and shirts designed for bold self-expression, giving you the perfect balance of trend, comfort, and durability.</p>
+
+          <b className="text-gray-800 text-2xl text-center">Our Mission</b>
+
+          <p className="">Our mission is to redefine everyday fashion by offering meticulously crafted jeans, shirts, and apparel that combine superior quality, modern aesthetics, and long-lasting comfort. We are committed to creating clothing that not only elevates your personal style but also matches the pace of your life—strong in durability, rich in detail, and refined in character. With a focus on honest craftsmanship, continuous innovation, and customer-first values, we strive to inspire confidence, celebrate individuality, and build a brand that people trust, wear proudly, and return to again and again.</p>
+        </div>
+      </div>
+
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US?"} />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-sm mb-8">
+
+  <div className="border border-gray-300 px-12 py-4 flex flex-col gap-6 rounded-2xl">
+    <b className="">Quality Assurance:</b>
+    <p className="text-gray-600">
+      Our Quality Assurance process leaves no room for compromise—every stitch, fabric, and detail is rigorously inspected to ensure you receive clothing that meets the highest standards of durability, comfort, and craftsmanship.
+    </p>
+  </div>
+
+  <div className="border border-gray-300 px-12 py-4 flex flex-col gap-6 rounded-2xl">
+    <b className="">Customer Service:</b>
+    <p className="text-gray-600">
+      We’re committed to providing reliable, responsive support at every step, ensuring your questions are answered quickly and your shopping experience always feels personal and stress-free.
+    </p>
+  </div>
+
+  <div className="border border-gray-300 px-12 py-4 flex flex-col gap-6 rounded-2xl">
+    <b className="">Convenience:</b>
+    <p className="text-gray-600">
+      We focus on making your shopping experience effortless and smooth, offering easy navigation, quick support, and simple checkout so you can find what you need without wasting a single moment.
+    </p>
+  </div>
+
+</div>
+
     </div>
   )
 }
--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [ currentState, setCurrentState ] = useState('Sign Up');
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto pt-12 gap-4 text-gray-800 border-t'>
+      <div className="inline-flex items-center gap-2 mb-2 mt-8">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className='border-none h-0.5 w-8 bg-gray-800'/>
+      </div>
+
+      {currentState === 'Login' ? '' : <input type="text" className="w-full px-3 py-2 border border-gray-800 outline-none" placeholder='Name' required/>
+      }
+      <input type="email" className="w-full px-3 py-2 border border-gray-800 outline-none" placeholder='Email' required/>
+      <input type="password" className="w-full px-3 py-2 border border-gray-800 outline-none" placeholder='Password' required/>
+
+      <div className="w-full flex justify-between text-sm">
+        <p className="cursor-pointer text-blue-500 hover:text-blue-900 underline">Forgot your password?</p>
+        {
+          currentState === 'Login' ? <p onClick={()=> setCurrentState('Sign Up')} className="cursor-pointer text-blue-500 hover:text-blue-900 underline">Create account</p> : <p onClick={()=> setCurrentState('Login')} className="cursor-pointer text-blue-500 hover:text-blue-900 underline">Login Here</p>
+        }
+      </div>
+      <button className='bg-black hover:bg-black/80 text-white text-sm px-6 py-2 rounded-xl'>
+        {currentState === 'Login' ? 'Sign In' : 'Sign Up'}
+      </button>
+    </form>
   )
 }
 
