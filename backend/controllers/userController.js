a4f71f8900c8156f39794d2f6c91cff8e751540d
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-29 20:04:27 +0530

    4th

---
+++ backend/controllers/userController.js
@@ -0,0 +1,62 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken';
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
+}
+
+const loginUser = async (req, res) => {
+}
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const exits = await userModel.findOne({ email });
+
+        if (exits) {
+            return res.json({
+                success: false,
+                message: "User with this email already exists."
+            })
+        }
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: "Please enter a valid email."
+            })
+        }
+        if (password.length < 6) {
+            return res.json({
+                success: false,
+                message: "Please enter a strong password of minimum 6 characters."
+            })
+        }
+        const salt = await bcrypt.genSalt(8);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name, email, password: hashedPassword
+        });
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+        return res.json({
+            success: true,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.json({
+                success: false,
+                message: error.message
+            })
+    }
+}
+
+const adminLogin = async (req, res) => {
+}
+
+export { loginUser, registerUser, adminLogin }
