494a0773a12a34a198ce5e794238f1807e410baa
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-12-06 17:29:26 +0530

    7th

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.fields([{
+productRouter.post('/add', adminAuth,upload.fields([{
     name: 'image1',
     maxCount: 1
 },
@@ -19,7 +20,7 @@ productRouter.post('/add', upload.fields([{
     name: 'image4',
     maxCount: 1
 }]), addProduct);
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove', adminAuth,removeProduct);
 productRouter.post('/single', singleProduct);
 productRouter.get('/list', listProduct);
 
