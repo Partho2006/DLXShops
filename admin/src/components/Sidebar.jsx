7b07405df6e02e0c13d1657df9bf182b55f05af7
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-12-15 13:01:21 +0530

    9th

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -5,21 +5,21 @@ import { assets } from '../assets/assets2'
 const Sidebar = () => {
   return (
     <div className='w-[20%] min-h-screen border-r'>
-      <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[20px]">
+      <div className="flex flex-col gap-4 pt-6 pl-[10%] text-[20px]">
         
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to="/add">
            <img className='w-5 h-5' src={assets.add_icon} alt="" />
-           <p className="hidden md:block">Add Items</p>
+           <p className="hidden sm:block text-xs md:text-sm lg:text-lg font-bold">Add Items</p>
         </NavLink>
 
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to="/list">
            <img className='w-5 h-5' src={assets.order_icon} alt="" />
-           <p className="hidden md:block">List Items</p>
+           <p className="hidden sm:block text-xs md:text-sm lg:text-lg font-bold">List Items</p>
         </NavLink>
 
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg' to="/orders">
            <img className='w-5 h-5' src={assets.order_icon} alt="" />
-           <p className="hidden md:block">Orders</p>
+           <p className="hidden sm:block text-xs md:text-sm lg:text-lg font-bold">Orders</p>
         </NavLink>
       </div>
     </div>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,29 +3,102 @@ import { assets } from '../assets/assets2'
 
 const Add = () => {
   return (
-    <form>
+    <form className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className="mb-2 font-bold">Upload Image</p>
 
         <div className="flex gap-2">
           <label htmlFor="image1">
             <img className='w-25 ' src={assets.upload_area} alt="" />
-            <input type="file" id='image1' hidden/>
+            <input type="file" id='image1' hidden />
           </label>
           <label htmlFor="image2">
             <img className='w-25 ' src={assets.upload_area} alt="" />
-            <input type="file" id='image2' hidden/>
+            <input type="file" id='image2' hidden />
           </label>
           <label htmlFor="image3">
             <img className='w-25 ' src={assets.upload_area} alt="" />
-            <input type="file" id='image3' hidden/>
+            <input type="file" id='image3' hidden />
           </label>
           <label htmlFor="image4">
             <img className='w-25 ' src={assets.upload_area} alt="" />
-            <input type="file" id='image4' hidden/>
+            <input type="file" id='image4' hidden />
           </label>
         </div>
       </div>
+
+      <div className="w-full">
+        <p className="mb-2 font-bold">Product Name</p>
+        <input className='w-full max-w-500 px-3 py-2' type="text" name="" id="" placeholder='Type here' required />
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2 font-bold">Product Description</p>
+        <textarea className='w-full max-w-500 px-3 py-2' type="text" name="" id="" placeholder='Write content here' required />
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
+
+        <div className="w-full">
+          <p className="mb-2 font-bold">Product Category</p>
+          <select className="w-full px-4 py-2 border border-gray-300 rounded-md outline-none focus:ring-2 focus:ring-black focus:border-black transition">
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+
+        <div className="w-full">
+          <p className="mb-2 font-bold">Sub Category</p>
+          <select className="w-full px-4 py-2 border border-gray-300 rounded-md outline-none focus:ring-2 focus:ring-black focus:border-black transition">
+            <option value="Topwear">Topwear</option>
+            <option value="Bottomwear">Bottomwear</option>
+            <option value="Winter">Winter</option>
+          </select>
+        </div>
+
+        <div className="w-full">
+          <p className="mb-2 font-bold">Product Price (₹)</p>
+          <input
+            type="number"
+            placeholder="25"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md outline-none focus:ring-2 focus:ring-black focus:border-black transition"
+          />
+        </div>
+      </div>
+
+      <div className="">
+        <p className="mb-2 font-bold">Product Sizes</p>
+
+        <div className="flex gap-3">
+          <div className="">
+            <p className="bg-slate-300 px-3 py-1 cursor-pointer">S</p>
+          </div>
+          <div className="">
+            <p className="bg-slate-300 px-3 py-1 cursor-pointer">M</p>
+          </div>
+          <div className="">
+            <p className="bg-slate-300 px-3 py-1 cursor-pointer">L</p>
+          </div>
+          <div className="">
+            <p className="bg-slate-300 px-3 py-1 cursor-pointer">XL</p>
+          </div>
+          <div className="">
+            <p className="bg-slate-300 px-3 py-1 cursor-pointer">XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-2 mt-2 ">
+        <input type="checkbox" name="" id="bestseller" />
+        <label className='cursor-pointer font-bold' htmlFor="bestseller">
+          Add to bestseller
+        </label>
+      </div>
+
+      <button className="bg-gradient-to-r from-gray-800 to-black hover:from-black hover:to-gray-800 text-white font-semibold py-2 px-6 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 w-32">
+        ADD
+      </button>
     </form>
   )
 }
