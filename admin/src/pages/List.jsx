33bc74da80cef344e7d427880a66a919b1652709
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-12-21 17:48:55 +0530

    11th

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { backendUrl } from '../App'
+import { backendUrl, currency } from '../App'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const List = () => {
   const [list, setList] = useState([])
@@ -7,20 +9,48 @@ const List = () => {
   const fetchList = async () => {
     try {
       const response = await axios.get(backendUrl + '/api/product/list')
-      console.log(response.data);
+
+      if (response.data.success) {
+        setList(response.data.products)
+      } else {
+        toast.error(response.data.message)
+      }
     } catch (error) {
-      
+      console.log(error)
+      toast.error(error.message)
     }
   }
 
-  useEffect(()=> {
-
-  },[])
+  useEffect(() => {
+    fetchList()
+  }, [])
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-300 text-sm">
+          <b className="">Image</b>
+          <b className="">Name</b>
+          <b className="">Category</b>
+          <b className="">Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        {
+          list.map((item, index) => (
+            <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm" key={index}>
+              <img src={item.image[0]} alt="" className='w-12'/>
+              <p className="">{item.name}</p>
+              <p className="">{item.category}</p>
+              <p className="">{currency}{item.price}</p>
+              <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
