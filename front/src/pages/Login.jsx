4debe70e42a072485eb0c6e8085674aad1dfe14c
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-12-25 18:24:42 +0530

    11th

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     try {
-      
+      // hawa hawa ayy hawa mujko ura le
     } catch (error) {
       
     }
